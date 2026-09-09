ca36aa406e07ef58169faa3e731f44b21bba33d3
SARTHAKSSS-MAKER <sarthakpoojary2005@gmail.com>
2026-09-09 21:02:54 +0530

    update

---
+++ backend/main.py
@@ -0,0 +1,38 @@
+import os
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow React frontend to communicate with FastAPI backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "WarehouseAI Backend is running!"
+    }
+
+
+@app.post("/upload")
+async def upload_video(file: UploadFile = File(...)):
+
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+    return {
+        "message": "Video uploaded successfully!",
+        "filename": file.filename
+    }
